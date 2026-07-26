0ec2cdea3ddb205954d201cf5ba2cc0d9b5c0cea
Mario Briseno <mario.briseno7@gmail.com>
2026-07-25 19:32:44 -0600

    added standard db dependency to avoid repeating boilerplate

--- src/db/engine.py
+++ src/db/engine.py
@@ -5,6 +5,7 @@ from ..config import Settings
 
 
 def get_engine(db_settings: Settings):
+
     return create_engine(
         f"postgresql+psycopg2://{db_settings.database_user}:{db_settings.database_password}@{db_settings.database_url}:{db_settings.database_port}/{db_settings.database_db}"
     )
@@ -12,4 +13,9 @@ def get_engine(db_settings: Settings):
 
 db_engine = get_engine(Settings())
 
-Session = sessionmaker(bind=db_engine)
+SessionLocal = sessionmaker(bind=db_engine)
+
+
+def get_db():
+    with SessionLocal() as session:
+        yield session
